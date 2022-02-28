e586559c131c7eeb15fbb82abc4e942d0a899a02
Søren Ditlefsen <soeren_ditlefsen@hotmail.com>
2022-02-28 20:40:41 +0100

    Added logic for direction

--- src/App.js
+++ src/App.js
@@ -13,43 +13,53 @@ const Canvas = styled.div`
   left: 50%;
   margin: -400px 0 0 -400px;
 `
+function calcCoordinatesFromDirection(direction) {    
+  return  [Math.sin(direction), Math.cos(direction)]
+}
 
 function App() {
 
   const handleAnswerChange = (event) => {
-    if (event.charCode === 55) {
-      setTop(top == 780 ? 0 : top + 20)
-    }
-    else if (event.charCode === 56) {
-      setRight(left == 780 ? 0 : left + 20)
+    //  8 on numpad
+    if (event.charCode === 56) { 
+      setDirection(direction => direction === 0? 359 : direction - 1)
+    } // 7  on numpad
+    else if (event.charCode === 55) {
+      setDirection(direction => direction === 359? 0 : direction + 1)
     }
   };
 
   const move = () => {
+  
     if (intervalId) {
       clearInterval(intervalId)
       setIntervalId(0)
       return
     }
 
+ 
+
     const newIntervalId = setInterval(() => {      
-      setTop(top => top === 780 ? 0 : top + 1)
-      setRight(right => right === 780 ? 0 : right + 1)
-    },5);
+      const [deltaX, deltaY] = calcCoordinatesFromDirection(direction)
+      console.log(direction)
+      setX(x => x === 780 ? 0 : x + deltaX)
+      setY(y => y === 780 ? 0 : y + deltaY)
+    },1);
     setIntervalId(newIntervalId)
   };
 
+  const [x, setX] = useState(395);
+  const [y, setY] = useState(395);
 
-  const [top, setTop] = useState(0);
-  const [left, setRight] = useState(0);
   const [intervalId, setIntervalId] = useState(0);
+  const [direction, setDirection] = useState(0);
 
   return (
     <div className="App">
       <StyledButton onClick={move}>{intervalId? "Stop" : "Start"}</StyledButton>
       <div id="inner" tabindex="0" onKeyPress={handleAnswerChange}>
         <Canvas >
-          <Snake top={top} left={left}></Snake>
+          <Snake top={y} left={x}></Snake>
         </Canvas>
       </div>
 
